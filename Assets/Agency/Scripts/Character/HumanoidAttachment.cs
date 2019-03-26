7e787a01274d3ee4c9c53f8358456521598f5d21
Mark <markh4b@hotmail.com>
2019-03-26 20:00:39 +0100

    Pickups can be displayed on the character

---
+++ Assets/Agency/Scripts/Character/HumanoidAttachment.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+	Defines attachment points for items, gear, etc.
+ */
+public class HumanoidAttachment : MonoBehaviour
+{
+    public Transform attach_right_hand;
+}
--- Assets/Agency/Scripts/Game/Interactions/PickupInteraction.cs
+++ Assets/Agency/Scripts/Game/Interactions/PickupInteraction.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PickupInteraction : InteractionDescription
 {
+	[Tooltip("The object attaches at this position when held, relative to the parent transform.")]
+	public Vector3 attachment_position;
+	public Vector3 attachment_rotation;
+
+
 	public override void Reset()
 	{
 		this.description = "Pick up";
@@ -17,10 +22,30 @@ public class PickupInteraction : InteractionDescription
 
 		if (null != user)
 		{
-			gameObject.transform.SetParent(user.transform);
+			HumanoidAttachment attach_manager = user.GetComponent<HumanoidAttachment>();
+			if (null != attach_manager)
+			{
+				gameObject.transform.SetParent(attach_manager.attach_right_hand);
+				gameObject.transform.localPosition = attachment_position;
+				gameObject.transform.localEulerAngles = attachment_rotation;
+
+				Rigidbody physics = this.GetComponent<Rigidbody>();
+				if (null != physics)
+				{
+					physics.isKinematic = true;
+				}
+			}
+			//gameObject.transform.SetParent(user.transform);
 		}
 		// TODO: Add to inventory
 		//Destroy(gameObject);
+	}
+
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(transform.position + attachment_position, 0.01f);
 	}
 }
 

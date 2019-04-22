41f69f83752877023b69632d46c33f61c05a1bf3
Mark <markh4b@hotmail.com>
2019-04-22 19:55:19 +0200

    Added interaction type

---
+++ Assets/Agency/Scripts/Game/Interactables/InteractionType.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ Defines how you can interact with an item
+ */
+public enum InteractionType
+{
+    /** [E] Use: Use, dump body, etc. */
+    Use, 
+	/** [F] Pick up, swap, etc. */
+	Pickup,
+    /** [G] Manipulate */
+    Distraction,
+    /** [B] Drag, drop, etc. */
+    Body,
+    /** [Z] Retrieve item */
+    Case,
+    /** [T] Disguise */
+    Disguise
+}
+
